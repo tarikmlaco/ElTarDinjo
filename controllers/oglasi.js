1ca88bf06aec53bbe4fa970d22dc878953c8df61
tarikmlaco <tarikmlaco@gmail.com>
2015-04-06 01:53:24 +0200

    Oglasi insertovani

--- controllers/oglasi.js
+++ controllers/oglasi.js
@@ -96,19 +96,19 @@ var populateDB = function() {
         name: "APARTMAN NA BRECI",
         cijena: "300",
         description: "The aromas of fruit and spice give one a hint of the light drinkability of this lovely oglas, which makes an excellent complement to fish dishes.",
-        picture: "saint_cosme.jpg"
+        picture: "apartman1.jpg"
     },
     {
         name: "DUPLEX U VOGOŠĆI",
         cijena: "400",
         description: "A resurgence of interest in boutique vineyards has opened the door for this excellent foray into the dessert oglas market. Light and bouncy, with a hint of black truffle, this oglas will not fail to tickle the taste buds.",
-        picture: "lan_rioja.jpg"
+        picture: "apartman2.jpg"
     },
     {
         name: "STAN NA ALIPAŠINOM POLJU",
         cijena: "500",
         description: "The cache of a fine Cabernet in ones oglas cellar can now be replaced with a childishly playful oglas bubbling over with tempting tastes of black cherry and licorice. This is a taste sure to transport you back in time.",
-        picture: "margerum.jpg"
+        picture: "apartman3.jpg"
     }];
 
     db.collection('oglasi', function(err, collection) {
